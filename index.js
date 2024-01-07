3e89734cabe19ccf9d3c779ebe5d830ebaa4bc8a
arnabjoy123 <arnab99joy@gmail.com>
2024-01-07 21:52:52 +0530

    8th commit

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-function plan()
-{
- neww=document.getElementById("dollar");
- neww.textcontent=" &dollar;199.99 ";
-}
+// function plan()
+// {
+//  var neww=document.getElementsByClassName("annually");
+//  neww.textcontent=document.getElementsByClassName("monthly");
+// }
+
+
+
+
+function plan() {
+    // Get elements with the class "monthly"
+    var elements = document.getElementsByClassName("monthly");
+  
+    // Toggle the "hide-element" class for each element using forEach
+    Array.from(elements).forEach(function(element) {
+      element.classList.toggle("hide-element");
+    });
+  
+    // Get elements with the class "annually"
+    var elements2 = document.getElementsByClassName("annually");
+  
+    // Toggle the "hide-element" class for each element using forEach
+    Array.from(elements2).forEach(function(element) {
+      element.classList.toggle("hide-element");
+    });
+  }
+  
+  
+
+
+
+
+
+// click on checkbox
+
+// hide annually
+
+
+// display monthly
